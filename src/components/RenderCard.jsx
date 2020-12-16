d27cf33943e75ee487ec263aaacf0b1f63ac844d
Abdulrahman <abdulrahman.kazkaz1@gmail.com>
2020-12-16 06:54:28 +0200

    adding home page

---
+++ src/components/RenderCard.jsx
@@ -0,0 +1,23 @@
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
+
+export const RenderCard = ({ item }) => {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import { DISHES } from "../shared/dishes";
+
+import { RenderCard } from "../components/RenderCard";
+
 export const Home = () => {
+  const [dishes, setDishes] = useState([]);
+  const [promotion, setPromotion] = useState([]);
+  const [leaders, setLeaders] = useState([]);
+
+  useEffect(() => {
+    setDishes(DISHES.filter((dish) => dish.featured)[0]);
+    setPromotion(PROMOTIONS.filter((promo) => promo.featured)[0]);
+    setLeaders(LEADERS.filter((leader) => leader.featured)[0]);
+  }, []);
+
   return (
     <div className="container">
-      <h4>Home</h4>
+      <div className="row align-item-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={dishes} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={leaders} />
+        </div>
+      </div>
     </div>
   );
 };
