553523112462cf1e10cd0c6118ee689e195997c4
Abdulrahman <abdulrahman.kazkaz1@gmail.com>
2020-12-26 18:51:18 +0200

    fixing null rating value

--- src/components/modal/CommentModal.jsx
+++ src/components/modal/CommentModal.jsx
@@ -1,7 +1,7 @@
-import { LocalForm, Control, Errors } from "react-redux-form";
-import { textValidation } from "../../helpers/validation";
-import { useDispatch } from "react-redux";
-import { addComment } from "../../redux/actions";
+import { LocalForm, Control, Errors } from 'react-redux-form';
+import { textValidation } from '../../helpers/validation';
+import { useDispatch } from 'react-redux';
+import { addComment } from '../../redux/actions';
 import {
   Modal,
   ModalBody,
@@ -10,7 +10,7 @@ import {
   Label,
   Col,
   Button,
-} from "reactstrap";
+} from 'reactstrap';
 
 export const CommentModal = ({ ModalIsOpen, toggleModal, dishId }) => {
   const validText = textValidation(2, 12, true);
@@ -18,7 +18,11 @@ export const CommentModal = ({ ModalIsOpen, toggleModal, dishId }) => {
 
   const handleLogin = (value) => {
     toggleModal();
-    dispatch(addComment(dishId, value.name, value.comment, value.rating));
+    let rating = value?.rating;
+    if (!rating) {
+      rating = 1;
+    }
+    dispatch(addComment(dishId, value.name, value.comment, rating));
   };
 
   return (
@@ -26,63 +30,63 @@ export const CommentModal = ({ ModalIsOpen, toggleModal, dishId }) => {
       <ModalHeader toggle={toggleModal}>Submit Comment</ModalHeader>
       <ModalBody>
         <LocalForm onSubmit={(val) => handleLogin(val)}>
-          <Row className="form-group">
-            <Label htmlFor="rating" md={12}>
+          <Row className='form-group'>
+            <Label htmlFor='rating' md={12}>
               Rating
             </Label>
             <Col md={12}>
               <Control.select
-                model=".rating"
-                name="rating"
-                id="rating"
-                className="form-control"
+                model='.rating'
+                name='rating'
+                id='rating'
+                className='form-control'
               >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
               </Control.select>
             </Col>
           </Row>
-          <Row className="form-group">
-            <Label htmlFor="name" md={12}>
+          <Row className='form-group'>
+            <Label htmlFor='name' md={12}>
               Your Name
             </Label>
             <Col md={12}>
               <Control.text
-                model=".name"
-                id="name"
-                name="name"
-                placeholder="Your Name"
-                className="form-control"
+                model='.name'
+                id='name'
+                name='name'
+                placeholder='Your Name'
+                className='form-control'
                 validators={validText.role}
               />
               <Errors
-                className="text-danger"
-                model=".name"
-                show="touched"
+                className='text-danger'
+                model='.name'
+                show='touched'
                 messages={validText.messages}
               />
             </Col>
           </Row>
-          <Row className="form-group">
-            <Label htmlFor="comment" md={12}>
+          <Row className='form-group'>
+            <Label htmlFor='comment' md={12}>
               Comment
             </Label>
             <Col md={12}>
               <Control.textarea
-                model=".comment"
-                id="comment"
-                name="comment"
-                rows="5"
-                className="form-control"
+                model='.comment'
+                id='comment'
+                name='comment'
+                rows='5'
+                className='form-control'
               />
             </Col>
           </Row>
-          <Row className="form-group">
+          <Row className='form-group'>
             <Col md={12}>
-              <Button type="submit" color="primary">
+              <Button type='submit' color='primary'>
                 Submit
               </Button>
             </Col>
