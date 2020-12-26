fe7759f26b9ee32456377b2bbc2cb9f1854eae8f
Abdulrahman <abdulrahman.kazkaz1@gmail.com>
2020-12-26 18:51:51 +0200

    fixing add comment action

--- src/redux/reducers/comments.js
+++ src/redux/reducers/comments.js
@@ -9,15 +9,16 @@ export const commentsReducers = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.ADD_COMMENT: {
       let comment = action.payload;
-      comment.id = state.length;
+      comment.id = state.comments.length;
       comment.date = new Date().toISOString();
-      return state.comments.comconcat(comment);
+      return { ...state, comments: state.comments.concat(comment) };
     }
     case ActionType.COMMENTS_FAILD: {
       return { ...state, err: action.payload, comments: null };
     }
-    case ActionType.ADD_COMMENTS:
-      return { ...state, err: null, dishes: action.payload };
+    case ActionType.ADD_COMMENTS: {
+      return { ...state, err: null, comments: action.payload };
+    }
     default: {
       return state;
     }
