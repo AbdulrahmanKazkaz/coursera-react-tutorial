b65ba4175e12acf375ab11aab85d75fc40af4a18
Abdulrahman <abdulrahman.kazkaz1@gmail.com>
2020-12-26 18:52:24 +0200

    fixing loading dish & fix get comments

--- src/components/menu/DishDetails.jsx
+++ src/components/menu/DishDetails.jsx
@@ -1,11 +1,11 @@
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 export const DishDetails = ({ dish }) => {
   return (
     <Card>
-      <CardImg width="100%" src={dish?.image} alt={dish?.name} />
+      <CardImg width='100%' src={dish?.image} alt={dish?.name} />
       <CardBody>
-        <CardTitle tag="h4">{dish?.name}</CardTitle>
+        <CardTitle tag='h4'>{dish?.name}</CardTitle>
         <CardText>{dish?.description}</CardText>
       </CardBody>
     </Card>
--- src/pages/MenuItem.jsx
+++ src/pages/MenuItem.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchDishes } from "../redux/actions";
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDishes, fetchComments } from '../redux/actions';
 // Import Menu Components
-import { DishDetails } from "../components/menu/DishDetails";
-import { DishComments } from "../components/menu/DishComments";
-import { BreadCrumb } from "../components/BreadCrumb";
-import { Loading } from "../components/Loading";
+import { DishDetails } from '../components/menu/DishDetails';
+import { DishComments } from '../components/menu/DishComments';
+import { BreadCrumb } from '../components/BreadCrumb';
+import { Loading } from '../components/Loading';
 
 export const MenuItem = ({ match }) => {
   const dispatch = useDispatch();
   const [dish, setDish] = useState(null);
+  const [Allcomments, setAllComments] = useState(null);
 
   const dishes = useSelector((state) => state.dishes);
-  const comments = useSelector((state) => state.comments).filter(
-    (comment) => comment.dishId == match.params.dishId
-  );
+  const comments = useSelector((state) => state.comments);
 
   useEffect(() => {
     dispatch(fetchDishes());
+    dispatch(fetchComments());
   }, []);
 
   useEffect(() => {
@@ -26,29 +26,36 @@ export const MenuItem = ({ match }) => {
         dishes.dishes.filter((dish) => dish.id == match.params.dishId)[0]
       );
     }
-  }, [dishes.dishes]);
+    if (comments.comments) {
+      setAllComments(
+        comments.comments.filter(
+          (comment) => comment.dishId == match.params.dishId
+        )
+      );
+    }
+  }, [dishes.dishes, comments.comments]);
 
   return (
     <>
-      {dish && (
-        <div className="container">
+      {dish && Allcomments && (
+        <div className='container'>
           <BreadCrumb menuComponent={dish?.name} />
-          <div className="row m-1">
-            <div className="col-12 col-md-6">
+          <div className='row m-1'>
+            <div className='col-12 col-md-6'>
               <DishDetails dish={dish} />
             </div>
-            <div className="col-12 col-md-6 pt-1">
+            <div className='col-12 col-md-6 pt-1'>
               <h3>Comments </h3>
               <hr />
-              {comments && (
-                <DishComments comments={comments} dishId={dish.id} />
+              {Allcomments && (
+                <DishComments comments={Allcomments} dishId={dish.id} />
               )}
             </div>
           </div>
         </div>
       )}
-      {dishes.isLoading && (
-        <div className="row">
+      {dishes.isLoading && (!dish || !Allcomments) && (
+        <div className='row'>
           <Loading />
         </div>
       )}
